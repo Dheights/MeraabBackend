c3aa5294107f61d2c082469ba4903779e5a1a9eb
bansalakshit <akshit@Akshits-MacBook-Pro.local>
2025-12-02 18:56:56 +0530

    fix app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
+import { AppController } from 'app.controller';
+import { AppService } from 'app.service';
 
 @Module({
   imports: [
@@ -27,5 +29,8 @@ import { UsersModule } from './modules/users/users.module';
 
     UsersModule,
   ],
+
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
